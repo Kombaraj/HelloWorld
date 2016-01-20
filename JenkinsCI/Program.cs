9e9ccf644fe90137d9300754dcc35e4065235b35
Kombaraj <kombaraj@gmail.com>
2016-01-20 14:39:28 +0000

    first commit

---
+++ JenkinsCI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JenkinsCI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(CreateMessage());
+        }
+
+        public static string CreateMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
